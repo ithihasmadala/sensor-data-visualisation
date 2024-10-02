b929a6bea45d039657b722ba814b4513ee249818
ritvik puranik <ritvikpuranik@gmail.com>
2024-10-02 12:17:24 +0530

    Revert "navbar template created"
    This reverts commit 55deedc00c323c344747f88b70761a7f0adf6f0f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import { Flex, Layout } from 'antd';
-import Navbar from './components/Navbar';
+import './App.css';
 
-const { Footer, Content } = Layout;
-
-const contentStyle: React.CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#0958d9',
-};
-
-const footerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#4096ff',
-};
-
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-};
-
-const App: React.FC = () => (
-  <Flex gap="middle" wrap>
-    <Layout style={layoutStyle}>
-      <Navbar />
-      <Content style={contentStyle}>Content</Content>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-  </Flex>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World 14563455 
+        </p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC<any> = () => {
     <>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: '1rem' }}>
         <h1 style={{ textAlign: 'left', margin: '0', color: '#333' }}>
-          Sensor Data Viz1
+          Sensor Data Viz
         </h1>
       </div>
 
