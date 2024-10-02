13bcafade701a6c95c05dd385cdd393b7027396f
ritvik puranik <ritvikpuranik@gmail.com>
2024-10-02 12:14:21 +0530

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC<any> = () => {
     <>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: '1rem' }}>
         <h1 style={{ textAlign: 'left', margin: '0', color: '#333' }}>
-          Sensor Data Viz
+          Sensor Data Viz1
         </h1>
       </div>
 
